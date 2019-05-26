20a2181b0804e650f1bfb98e38c33200dc2cb550
qwertyuiopasdfghk <48489690+qwertyuiopasdfghk@users.noreply.github.com>
2019-05-26 15:19:03 -0500

    se crearon los values y los paquetes con las clases

---
+++ app/src/main/java/com/utils/RecyclerTouchListener.java
@@ -0,0 +1,12 @@
+package com.utils;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.view.View;
+
+public class RecyclerTouchListener {
+
+
+}
